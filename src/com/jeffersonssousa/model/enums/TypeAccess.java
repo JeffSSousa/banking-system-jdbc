b499f2ed0f427a6430a40f6b75b82ab1d73f1da3
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2025-06-17 15:04:35 -0300

    fix: update Enums

--- src/com/jeffersonssousa/model/enums/TypeAccess.java
+++ src/com/jeffersonssousa/model/enums/TypeAccess.java
@@ -5,6 +5,22 @@ public enum TypeAccess {
 	ADMINISTRADOR(1),
 	CLIENTE(2);
 
-	TypeAccess(int i) {
-	}
+	private int id;
+
+    private TypeAccess(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static TypeAccess valueOf(int id) {
+        for (TypeAccess type : TypeAccess.values()) {
+            if (type.getId() == id) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Id de TypeTransaction inválido: " + id);
+    }
 }
--- src/com/jeffersonssousa/model/enums/TypeAccount.java
+++ src/com/jeffersonssousa/model/enums/TypeAccount.java
@@ -6,6 +6,22 @@ public enum TypeAccount {
 	POUPANCA(1),
 	CORRENTE(2);
 
-	TypeAccount(int i) {
-	}
+	private int id;
+
+    private TypeAccount(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static TypeAccount valueOf(int id) {
+        for (TypeAccount type : TypeAccount.values()) {
+            if (type.getId() == id) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Id de TypeTransaction inválido: " + id);
+    }
 }

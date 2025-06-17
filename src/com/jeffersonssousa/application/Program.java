c9cc87f3e9e3fb7481a1636110ab5b4d1a4d108c
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2025-06-16 23:47:52 -0300

    feat: created Administrator Menu

--- src/com/jeffersonssousa/application/Program.java
+++ src/com/jeffersonssousa/application/Program.java
@@ -2,19 +2,29 @@ package com.jeffersonssousa.application;
 
 import java.util.Scanner;
 
+import com.jeffersonssousa.view.menu.AdministratorView;
 import com.jeffersonssousa.view.menu.HomeMenuView;
-import com.jeffersonssousa.view.util.Screen;
 
 public class Program {
 	public static void main(String[] args) {
 
-		Scanner  sc = new Scanner(System.in);
-		
-		int option = HomeMenuView.showHomeMenu(sc);
-		
-		System.out.println(option);
-		Screen.clearScreen();
-		
+		Scanner sc = new Scanner(System.in);
+
+		int option ;
+
+		do {
+			option = HomeMenuView.showHomeMenu(sc);
+			switch (option) {
+			case 1:
+				AdministratorView.showAdministratorMenu(sc);
+				break;
+
+			case 0:
+				System.out.println("Saindo...");
+			}
+
+		} while (option != 0);
+
 		sc.close();
 	}
 }
---
+++ src/com/jeffersonssousa/view/menu/AdministratorView.java
@@ -0,0 +1,18 @@
+package com.jeffersonssousa.view.menu;
+
+import java.util.Scanner;
+
+public class AdministratorView {
+
+	public static int showAdministratorMenu(Scanner sc) {
+		System.out.println("=== MENU ADMINISTRADOR ===");
+		System.out.println("1. Listar todos os clientes");
+		System.out.println("2. Listar todas as contas");
+		System.out.println("3. Apagar Acesso");
+		System.out.println("4. Procurar Cliente/Conta");
+		System.out.println("5. Voltar ao menu principal");
+
+		return sc.nextInt();
+	}
+
+}

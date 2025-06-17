398a2fe05ea2c1a402b9884020bb13c745e6baaa
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2025-06-17 00:03:50 -0300

    feat: created method exitTheScreen

--- src/com/jeffersonssousa/application/Program.java
+++ src/com/jeffersonssousa/application/Program.java
@@ -3,28 +3,32 @@ package com.jeffersonssousa.application;
 import java.util.Scanner;
 
 import com.jeffersonssousa.view.menu.AdministratorView;
+import com.jeffersonssousa.view.menu.ClientMenuView;
 import com.jeffersonssousa.view.menu.HomeMenuView;
+import com.jeffersonssousa.view.util.Screen;
 
 public class Program {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 
-		int option ;
+		int option;
 
 		do {
+			Screen.clearScreen();
 			option = HomeMenuView.showHomeMenu(sc);
+
 			switch (option) {
 			case 1:
 				AdministratorView.showAdministratorMenu(sc);
 				break;
 
-			case 0:
-				System.out.println("Saindo...");
+			case 2:
+				ClientMenuView.showClientMenu(sc);
 			}
 
 		} while (option != 0);
-
-		sc.close();
+		
+		Screen.exitTheSystem(sc);
 	}
 }
--- src/com/jeffersonssousa/view/util/Screen.java
+++ src/com/jeffersonssousa/view/util/Screen.java
@@ -1,9 +1,18 @@
 package com.jeffersonssousa.view.util;
 
+import java.util.Scanner;
+
 public class Screen {
 	
 	public static void clearScreen() {
 		System.out.print("\033[H\033[2J");
 		System.out.flush();
+	}
+	
+	public static void exitTheSystem(Scanner sc) {
+		System.out.println("Saindo...");
+		if(sc != null) {
+			sc.close();
+		}
 	}
 }

b845f6c4d184295d84b2487ba8a2534242d6ed11
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2025-06-17 00:03:04 -0300

    feat: created ClientMenuView

---
+++ src/com/jeffersonssousa/view/menu/ClientMenuView.java
@@ -0,0 +1,19 @@
+package com.jeffersonssousa.view.menu;
+
+import java.util.Scanner;
+
+public class ClientMenuView {
+
+	public static int showClientMenu(Scanner sc) {
+		System.out.println("=== MENU CLIENTE ===");
+		System.out.println("1. Ver dados da conta");
+		System.out.println("2. Consultar saldo");
+		System.out.println("3. Depositar valor");
+		System.out.println("4. Sacar valor");
+		System.out.println("5. Transferir para outra conta");
+		System.out.println("6. Ver extrato");
+		System.out.println("7. Sair");
+
+		return sc.nextInt();
+	}
+}

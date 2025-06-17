2c9c6c00504e3a9fd5a9448a5811713246a967dc
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2025-06-17 11:27:25 -0300

    feat: created accountRepository

--- src/com/jeffersonssousa/application/Program.java
+++ src/com/jeffersonssousa/application/Program.java
@@ -1,7 +1,12 @@
 package com.jeffersonssousa.application;
 
+import java.sql.Connection;
 import java.util.Scanner;
 
+import com.jeffersonssousa.config.connection.DBConnection;
+import com.jeffersonssousa.model.entities.Account;
+import com.jeffersonssousa.repository.AccountRepository;
+import com.jeffersonssousa.repository.ClientRepository;
 import com.jeffersonssousa.view.menu.AdministratorView;
 import com.jeffersonssousa.view.menu.ClientMenuView;
 import com.jeffersonssousa.view.menu.HomeMenuView;
@@ -10,6 +15,32 @@ import com.jeffersonssousa.view.util.Screen;
 public class Program {
 	public static void main(String[] args) {
 
+		Connection conn = DBConnection.getConnection();
+		
+		Account newAcc = new Account(null, 12345, "Jef123", 6000.0, 1, 1);
+		
+		AccountRepository rep = new AccountRepository(conn);
+		ClientRepository repC = new ClientRepository(conn); 
+		
+		
+		System.out.println("Find By All");
+		rep.findAll().forEach(System.out::println);
+		System.out.println();
+		System.out.println("Find By Id");
+		System.out.println(rep.findById(1));
+		System.out.println();
+		System.out.println("Insert");
+		rep.insert(newAcc);
+		System.out.println();
+		System.out.println("delete");
+		rep.delete(3);
+		System.out.println();
+		System.out.println("update");
+		rep.update(newAcc);
+		
+	}
+
+	public static void testScreen() {
 		Scanner sc = new Scanner(System.in);
 
 		int option;
@@ -28,7 +59,7 @@ public class Program {
 			}
 
 		} while (option != 0);
-		
+
 		Screen.exitTheSystem(sc);
 	}
 }
---
+++ src/com/jeffersonssousa/repository/AccountRepository.java
@@ -0,0 +1,130 @@
+package com.jeffersonssousa.repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jeffersonssousa.config.exceptions.DatabaseException;
+import com.jeffersonssousa.model.entities.Account;
+
+public class AccountRepository implements BaseRepository<Account> {
+
+	private Connection conn;
+
+	public AccountRepository(Connection conn) {
+		this.conn = conn;
+	}
+
+	private Account instantiateAccount(ResultSet rs) throws SQLException {
+		Account obj = new Account();
+		obj.setId(rs.getInt("id"));
+		obj.setAccountNumber(rs.getInt("account_number"));
+		obj.setPassword(rs.getString("password"));
+		obj.setBalance(rs.getDouble("balance"));
+		obj.setTypeAccountId(rs.getInt("type_account_id"));
+		obj.setClientId(rs.getInt("tb_client_id"));
+		return obj;
+	}
+
+	@Override
+	public void insert(Account obj) {
+		String sql = "INSERT INTO tb_account " + "(account_number, password, balance, type_account_id, tb_client_id)" + "values"
+				+ "( ?, ?, ?, ?, ?)";
+
+		try (PreparedStatement st = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+			st.setInt(1, obj.getAccountNumber());
+			st.setString(2, obj.getPassword());
+			st.setDouble(3, obj.getBalance());
+			st.setInt(4, obj.getTypeAccountId());
+			st.setInt(5, obj.getClientId());
+
+			int rowsAffected = st.executeUpdate();
+
+			if (rowsAffected > 0) {
+				try (ResultSet rs = st.getGeneratedKeys()) {
+					if (rs.next()) {
+						int id = rs.getInt(1);
+						obj.setId(id);
+					}
+				}
+			} else {
+				throw new DatabaseException("Nenhuma linha foi inserida");
+			}
+
+			System.out.println("Conta Inserida no banco com Sucesso");
+
+		} catch (SQLException e) {
+			throw new DatabaseException("Erro ao inserir uma Conta: " + e.getMessage());
+		}
+	}
+
+	@Override
+	public void update(Account obj) {
+		throw new DatabaseException("Não é permitido alterar dados da Conta");
+	}
+
+	@Override
+	public void delete(Integer id) {
+		String sql = "DELETE FROM tb_account " + "WHERE id = ?";
+
+		try (PreparedStatement st = conn.prepareStatement(sql)) {
+			st.setInt(1, id);
+
+			int rowsAffacted = st.executeUpdate();
+
+			if (rowsAffacted == 0) {
+				throw new DatabaseException(
+						"Nenhuma Conta com o ID: " + id + " foi encontrado para ser deletado do banco de dados");
+			}
+
+			System.out.println("Conta deletado com sucesso");
+
+		} catch (SQLException e) {
+			throw new DatabaseException("Erro ao deletar uma Conta do banco de dados: " + e.getMessage());
+		}
+	}
+
+	@Override
+	public Account findById(Integer id) {
+		String sql = "SELECT * FROM system_bank.tb_account" + " WHERE id = ?";
+
+		try (PreparedStatement st = conn.prepareStatement(sql)) {
+			st.setInt(1, id);
+
+			try (ResultSet rs = st.executeQuery()) {
+				if (rs.next()) {
+					return instantiateAccount(rs);
+				} else {
+					throw new DatabaseException("Conta do ID: " + id + " não foi encontrado no Banco de Dados!!");
+				}
+			}
+
+		} catch (SQLException e) {
+			throw new DatabaseException("Erro ao procurar cliente por Id: " + e.getMessage());
+		}
+	}
+
+	
+	@Override
+	public List<Account> findAll() {
+		List<Account> list = new ArrayList<>();
+
+		String sql = "SELECT * FROM system_bank.tb_account";
+
+		try (PreparedStatement st = conn.prepareStatement(sql); ResultSet rs = st.executeQuery()) {
+
+			while (rs.next()) {
+				list.add(instantiateAccount(rs));
+			}
+
+		} catch (SQLException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		return list;
+	}
+
+}

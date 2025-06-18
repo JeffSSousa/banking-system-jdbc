2d7eb27ed981125fbbaf3fbb06dd759495110d4a
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2025-06-18 14:12:28 -0300

    fix: Controller added in AccountDetailsView

--- src/com/jeffersonssousa/application/Program.java
+++ src/com/jeffersonssousa/application/Program.java
@@ -10,15 +10,11 @@ import com.jeffersonssousa.view.util.Screen;
 public class Program {
 	public static void main(String[] args) {
 
-		AccountController acc = new AccountController();
-		ClientController client = new ClientController();
-		
-		System.out.println(acc.viewAccountdetails(1));
-		System.out.println(client.viewCustomerData(1));
+		testScreen();
 		
 	}
 
-	public static void testScree() {
+	public static void testScreen() {
 		Scanner sc = new Scanner(System.in);
 		try {
 			ClientMenuView.startNavigation(sc);
--- src/com/jeffersonssousa/view/menu/client/AccountDetailsView.java
+++ src/com/jeffersonssousa/view/menu/client/AccountDetailsView.java
@@ -2,15 +2,25 @@ package com.jeffersonssousa.view.menu.client;
 
 import java.util.Scanner;
 
+import com.jeffersonssousa.controller.AccountController;
+import com.jeffersonssousa.controller.ClientController;
+import com.jeffersonssousa.model.enums.TypeAccount;
+
 public class AccountDetailsView {
 	
 	public static int showDetails(Scanner sc) {
+		
+		ClientController client = new ClientController();
+		AccountController account = new AccountController();
+		
 		System.out.println("=== DADOS DA CONTA ===");
-		System.out.println("Nome: --------");
-		System.out.println("Email: fulando@gmail.com");
-		System.out.println("CPF: 123.456.789-20");
-		System.out.println("Numero da Conta: 12345-6");
-		System.out.println("Tipo da Conta: Conta Corrente");
+		System.out.println("Nome: " + client.viewCustomerData(1).getFirstName() + " " + client.viewCustomerData(1).getLastName());
+		System.out.println("Email: " + client.viewCustomerData(1).getEmail());
+		System.out.println("CPF: " + client.viewCustomerData(1).getCpf());
+		System.out.println("Data de Nascimento: " + client.viewCustomerData(1).getBirthDate());
+		System.out.println();
+		System.out.println("Numero da Conta: " + account.viewAccountdetails(1).getAccountNumber());
+		System.out.println("Tipo da Conta: " + TypeAccount.valueOf(account.viewAccountdetails(1).getTypeAccountId()));
 		return sc.nextInt();
 	}
 

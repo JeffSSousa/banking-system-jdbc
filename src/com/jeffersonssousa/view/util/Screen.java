c6d7ad88aeded7f5716dfbc0996cef92dd17ad66
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2025-06-24 14:22:04 -0300

    fix: fix method clearScreen of class Screen

--- src/com/jeffersonssousa/view/util/Screen.java
+++ src/com/jeffersonssousa/view/util/Screen.java
@@ -7,8 +7,9 @@ import java.util.Scanner;
 public class Screen {
 
 	public static void clearScreen() {
-		System.out.print("\033[H\033[2J");
-		System.out.flush();
+		for (int i = 0; i < 50; i++) {
+	        System.out.println();
+	    }
 	}
 
 	public static void showReturnToMenu(String msg) {
